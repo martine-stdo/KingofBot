f75b3510b7ee622777195da37db4aa0b5798241b
martine-stdo <2062461560@qq.com>
2023-05-14 11:52:15 +0800

    解决了跨域问题

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,8 +1,43 @@
 <template>
-  <div>Hello, World!</div>
+  <div>
+    <div>Bot昵称: {{ bot_name }}</div>
+    <div>Bot战力: {{ bot_rating }}</div>
+  </div>
+
   <router-view/>
 </template>
 
-<style>
+<script>
+import $ from 'jquery';
+import { ref } from 'vue';
+export default {
+  name: "App",
+  setup: () => {
+    let bot_name = ref("");
+    let bot_rating = ref("");
 
+    $.ajax({
+      url:"http://localhost:3001/pk/getbotinfo/",
+      type: "get",
+      success: resp => {
+       bot_name.value = resp.name;
+       bot_rating.value = resp.rating;
+      }
+    })
+
+    return {
+      bot_name,
+      bot_rating
+    }
+  }
+}
+
+</script>
+
+<style>
+body {
+  background-image: url("https://s1.ax1x.com/2022/10/16/xBcp6S.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+}
 </style>
