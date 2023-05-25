fc7b09f3b1a3d8099f302e58696f5db77573a53e
martine-stdo <2062461560@qq.com>
2023-05-25 22:43:28 +0800

    使得地图关于中心对称

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -10,7 +10,7 @@ export class GameMap extends AcGameObject {
         this.L = 0;
 
         this.rows = 13;
-        this.cols = 13;
+        this.cols = 14;
 
         this.inner_walls_count = 20;
         this.walls = [];
@@ -57,11 +57,11 @@ export class GameMap extends AcGameObject {
                 let r = parseInt(Math.random() * this.rows);
                 let c = parseInt(Math.random() * this.cols);
 
-                if (g[r][c] || g[c][r]) continue;
+                if (g[r][c] || g[this.rows - 1 - r][this.cols - 1 - c]) continue;
 
                 if (r == this.rows - 2 && c == 1 || r == 1 && c == this.cols - 2) continue;
 
-                g[r][c] = g[c][r] = true;
+                g[r][c] = g[this.rows - 1 - r][this.cols - 1 - c] = true;
                 break;
             }
         }
