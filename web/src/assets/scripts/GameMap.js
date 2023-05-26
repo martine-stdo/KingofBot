c99fa1d5e00c5a85393a2879982987732dcd97c4
martine-stdo <2062461560@qq.com>
2023-05-26 16:35:37 +0800

    完成非法判断

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -1,4 +1,5 @@
 import { AcGameObject } from "./AcGameObject";
+import { Snake } from "./Snake";
 import { Walls } from "./Walls";
 
 export class GameMap extends AcGameObject {
@@ -14,6 +15,14 @@ export class GameMap extends AcGameObject {
 
         this.inner_walls_count = 20;
         this.walls = [];
+
+        this.snakes = [
+            new Snake({id: 0, color:"rgb(75, 118, 236)", r: this.rows - 2, c: 1}, this),
+            new Snake({id: 1, color:"rgb(249, 71, 73)", r: 1, c: this.cols - 2}, this),
+        ];
+
+       
+
     }
 
     check_connectivity(g, sx, sy, tx, ty) {
@@ -82,18 +91,72 @@ export class GameMap extends AcGameObject {
 
     }
 
+    add_listening_events() {
+        this.ctx.canvas.focus();
+
+        const [snake0, snake1] = this.snakes;
+
+        this.ctx.canvas.addEventListener("keydown", (e) => {
+            if (e.key === 'w') snake0.set_direction(0);
+            if (e.key === 'd') snake0.set_direction(1);
+            if (e.key === 's') snake0.set_direction(2);
+            if (e.key === 'a') snake0.set_direction(3);
+            if (e.key === 'ArrowUp') snake1.set_direction(0);
+            if (e.key === 'ArrowRight') snake1.set_direction(1);
+            if (e.key === 'ArrowDown') snake1.set_direction(2);
+            if (e.key === 'ArrowLeft') snake1.set_direction(3);
+        });
+    }
+
     start() { 
        for (let i = 0; i < 1000; i ++) {
               if (this.create_walls()) break;
        }
+       this.add_listening_events();
     }
     update_size() {
         this.L = parseInt(Math.min(this.parent.clientWidth / this.cols, this.parent.clientHeight / this.rows));
         this.ctx.canvas.width = this.L * this.cols;
         this.ctx.canvas.height = this.L * this.rows;
     }
+
+    check_ready() { //判断两条蛇是否都准备好下一回合了
+        for (const snake of this.snakes) {
+            if (snake.status !== "idle") return false;
+            if (snake.direction === -1) return false;
+        }
+        return true;
+    }
+
+    next_step() { //让两条蛇进入下一回合
+        for (const snake of this.snakes) {
+            snake.next_step();
+        }
+    }
+
+    check_vaild(cell) { //检测目标位置是否合法：没有撞到两条蛇的身体和墙
+        for (const wall of this.walls) {
+            if (wall.r === cell.r && wall.c === cell.c) return false;
+        }
+
+        for (const snake of this.snakes) {
+            let k = snake.cells.length;
+            if (!snake.check_tail_increasing()) {//当蛇尾会前进的时候，蛇尾不要判断
+                k --;
+            }
+
+            for (let i = 0; i < k; i ++) {
+                if (snake.cells[i].r === cell.r && snake.cells[i].c === cell.c) return false;
+            }
+        }
+        return true;
+    }
+
     update() { 
         this.update_size();
+        if (this.check_ready()) {
+            this.next_step();
+        }
         this.render();
     }
 
