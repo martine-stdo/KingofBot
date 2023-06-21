f20b073204e317168e0cde7645d5b606912f3320
martine-stdo <2062461560@qq.com>
2023-06-21 13:19:36 +0800

    实现了Bot的增删改查接口

--- web/src/views/users/bot/UserBotindexView.vue
+++ web/src/views/users/bot/UserBotindexView.vue
@@ -6,9 +6,70 @@
     
     <script>
     import ContentField from '../../../components/ContentField.vue'
+    import $ from 'jquery';
+    import { useStore } from 'vuex';
+
     export default {
         components: {
             ContentField
+        },
+        setup() {
+            const store = useStore();
+            // $.ajax({
+            //     url: "http://127.0.0.1:3001/user/bot/add",
+            //     type: "post",
+            //     data: {
+            //         title: "Bot的标题",
+            //         description: "Bot的描述",
+            //         content: "Bot的代码",
+            //     },
+            //     headers: {
+            //         Authorization: "Bearer " + store.state.user.token,
+            //     },
+            //     success(resp) {
+            //         console.log(resp);
+            //     },
+            //     error(resp) {
+            //         console.log(resp);
+            //     },
+            // })
+
+            // $.ajax({
+            //     url: "http://127.0.0.1:3001/user/bot/remove",
+            //     type: "post",
+            //     data: {
+            //         bot_id : 2,
+            //     },
+            //     headers: {
+            //         Authorization: "Bearer " + store.state.user.token,
+            //     },
+            //     success(resp) {
+            //         console.log(resp);
+            //     },
+            //     error(resp) {
+            //         console.log(resp);
+            //     },
+            // })
+
+            $.ajax({
+                url: "http://127.0.0.1:3001/user/bot/update",
+                type: "post",
+                data: {
+                    bot_id : 3,
+                    title: "Bot修改测试",
+                    description: "Bot的描述",
+                    content: "Bot的代码",
+                },
+                headers: {
+                    Authorization: "Bearer " + store.state.user.token,
+                },
+                success(resp) {
+                    console.log(resp);
+                },
+                error(resp) {
+                    console.log(resp);
+                },
+            })
         }
     }
     
